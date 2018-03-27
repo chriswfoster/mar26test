d53ecbb3efa556fc642d0b269bd4ce4c40a3d250
ChriswFoster <chriswfoster@gmail.com>
2018-03-26 22:04:28 -0500

    Building out index

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ const cors = require('cors')
 const {json} = require('body-parser')
 
 
+// I didn't gitignore the .env so you can try it out for yourself :)
+
 const port = 3001
+
+const app = express()
+app.use(json())
+app.use(cors())
 
 
 
